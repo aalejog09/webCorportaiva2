2e659d603b1514b43b28f90c39483a0f139a2aaf
burgoslp <pinedo.burgoslp3@gmail.com>
2020-02-21 23:39:46 -0400

    clinicMedic version0.50 conectado a heroku x01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Oncovip Medicina preventiva Papanicolaou</title>
-        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{asset('css/font-style.css')}}">
-        <link rel="stylesheet" href="{{asset('css/propio.css')}}">
+        <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/font-style.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/propio.css') }}">
                 
     </head>
     <body>
